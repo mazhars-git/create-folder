f0f3a422566eb120c1a0d86e5768a7e009f82352
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-11-21 20:07:49 +0600

    update

--- src/Components/NewFolder/NewFolder.js
+++ src/Components/NewFolder/NewFolder.js
@@ -2,14 +2,37 @@ import React from 'react';
 
 const NewFolder = (props) => {
     const {name} = props.data;
+
+    const handleDelete = () =>{
+        console.log('Clicked : Delete')
+    }
+    const handleCreate = () =>{
+        console.log('Clicked : Create')
+    }
+
     return (
+        <>
         <div>
             <ul>
                 <li>
-                    {name}
+                    {name} 
+                    <button onClick={handleDelete}>X</button>
+                    <button onClick={handleCreate}>+</button>
                 </li>
             </ul>
         </div>
+
+        <div>
+            <form>
+                <label for="name">Create Folder <br/></label>
+                <input type="text" name='name' id='name' placeholder='Folder Name'/> <br />
+
+                <button type="button">Cancel</button> 
+                <button type="submit">Create</button>
+                
+            </form>
+        </div>
+        </>
     );
 };
 
